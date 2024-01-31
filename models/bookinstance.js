c2592f7b7c91b162759e4c205ea4a52483fd31c4
Paulo Victor <paulovictor.devs@gmail.com>
2024-01-31 16:41:05 -0300

    add library app with express and mongoose

---
+++ models/bookinstance.js
@@ -0,0 +1,42 @@
+const { DateTime } = require('luxon');
+
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const BookInstanceSchema = new Schema({
+  book: { type: Schema.Types.ObjectId, ref: 'Book', required: true }, // reference to the associated book
+  imprint: { type: String, required: true },
+  status: {
+    type: String,
+    required: true,
+    enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'],
+    default: 'Maintenance',
+  },
+  due_back: { type: Date, default: Date.now },
+});
+
+/*
+
+*** About enum: ***
+
+enum: This allows us to set the allowed values of a string. In this case, we use it to specify the availability status of our books (using an enum means that we can prevent mis-spellings and arbitrary values for our status).
+
+*/
+
+// Virtual for bookinstance's URL
+BookInstanceSchema.virtual('url').get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/catalog/bookinstance/${this._id}`;
+});
+
+BookInstanceSchema.virtual('due_back_formatted').get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
+});
+
+BookInstanceSchema.virtual('due_back_yyyy_mm_dd').get(function () {
+  return DateTime.fromJSDate(this.due_back).toISODate(); // format 'YYYY-MM-DD'
+});
+
+// Export model
+module.exports = mongoose.model('BookInstance', BookInstanceSchema);
